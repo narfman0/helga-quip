7fb517878af0e6d63f60d14dd4c729ea0561b0b7
Jon Robison <narfman0@gmail.com>
2015-01-14 16:56:54 -0500

    -Update to helga 1.6 shlex support

--- helga_quip/plugin.py
+++ helga_quip/plugin.py
@@ -1,40 +1,31 @@
-import re, requests
+import re
 from helga.db import db
 from helga.plugins import command, match, random_ack
 
 _help_text = 'Match quips and other witticisms. Usage:\
-1. helga quip phrase add/remove <quip_id> <quip_regex>\
-2. helga quip kind add/remove <quip_id> <quip_phrase>'
+1. helga quip add/remove <quip_kind> <quip_regex>'
 
 def _quip_manage(client, channel, nick, message, args):
     """ Add/remove quip/phrase to stash """
-    final = ' '.join(args[3:])
-    if args[0] == 'kind':
-        kind = {'kind':args[2], 'phrase':final}
-        if args[1] == 'add':
-            db.helga_quip.kind.insert(kind)
-        elif args[1] == 'remove':
-            db.helga_quip.kind.remove(kind)
-    elif args[0] == 'phrase':
-        phrase = {'kind':args[2], 'regex':final}
-        if args[1] == 'add':
-            phrase['nick'] = nick
-            db.helga_quip.phrases.insert(phrase)
-        elif args[1] == 'remove':
-            db.helga_quip.phrases.remove(phrase)
+    regex = '.*' + args[2] + '.*'
+    phrase = {'kind':args[1], 'regex':regex}
+    if args[0] == 'add':
+        phrase['nick'] = nick
+        db.helga_quip.entries.insert(phrase)
+    elif args[0] == 'remove':
+        db.helga_quip.entries.remove(phrase)
     return random_ack()
 
 def _quip_respond(message):
     """ Search for matching quip, respond if exists """
-    for phrase in db.helga_quip.phrases.find():
+    for phrase in db.helga_quip.entries.find():
         result = re.match(phrase['regex'], message)
         if result:
-            kind = db.helga_quip.kind.find_one({'kind':phrase['kind']})
-            return ('success', kind['phrase'] + ': ' + result.string)
+            return ('success', phrase['kind'])
     return ''
 
 @match(_quip_respond)
-@command('quip', aliases=['joke', 'quips'], help=_help_text)
+@command('quip', aliases=['joke', 'quips'], help=_help_text, shlex=True)
 def quip(client, channel, nick, message, *args):
     """ Helga endpoint for plugin """
     # hacky because this could be either match which already populated args or
@@ -43,4 +34,3 @@ def quip(client, channel, nick, message, *args):
         return _quip_manage(client, channel, nick, message, args[1])
     if args[0][0] == 'success':
         return args[0][1]
-
